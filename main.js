fe2d3eeed77befcd9b126ab58e0867f833508254
Doda <root.doda@gmail.com>
2023-09-19 14:27:11 +0200

    Linked the DOM in JS and added the basic variables

---
+++ main.js
@@ -0,0 +1,23 @@
+// Linking the DOM
+let clear = document.getElementById('Clear')
+let state = document.getElementById('State')
+let percent = document.getElementById('Percentage')
+let divide = document.getElementById('Divide')
+let times = document.getElementById('*')
+let minus = document.getElementById('-')
+let plus = document.getElementById('+')
+let equal = document.getElementById('=')
+let zero = document.getElementById('0')
+let one = document.getElementById('1')
+let two = document.getElementById('2')
+let three = document.getElementById('3')
+let four = document.getElementById('4')
+let five = document.getElementById('5')
+let six = document.getElementById('6')
+let seven = document.getElementById('7')
+let eight = document.getElementById('8')
+let nine = document.getElementById('9')
+
+// Global Variables
+
+// Logic Function
