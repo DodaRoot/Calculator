75c16fee126346a41b6cd201275034fb50a32d10
Dode Doda <root.doda@gmail.com>
2023-11-12 15:22:11 +0100

    Beter desighn and diferent logic

--- main.js
+++ main.js
@@ -1,359 +1,62 @@
-// Linking the DOM
-let placeHolder = document.querySelector('.screen')
+// Linking the dom
+let display = document.querySelector('.display')
+let btn = document.querySelectorAll('#btn')
+let op = 0
+let bool = false
+// Default display output
+let output = 0
 
-
-// Global Variables
-let operation = 0;
-let inputOne = '';
-let inputTwo = '';
-let operator = [];
-let formula = '';
-let output = 0;
-
-// Logic Function
-
-function one () {
-    inputOne = [];
-    inputTwo = [];
-    if (operation == 0) {
-        if (formula.length <= 5) {
-            inputOne += 1
-            formula += inputOne
-            logic()
-        } 
-    }
-    else if (operation == 1) {
-        if (formula.length <= 11) {
-            inputTwo += 1
-            formula += inputTwo
-            logic()
+// Geting the value from the calc buttons
+btn.forEach((item) => {
+    item.addEventListener('click', () => {
+        ['+', '-', '*', '/'].includes(item.value) ? bool = true : bool = false;
+        if (op == -1) {
+            display.innerText = '0'
+            op++
         }
-    }
-}
-
-function two () {
-    inputOne = [];
-    inputTwo = [];
-    if (operation == 0) {
-        if (formula.length <= 5) {
-            inputOne += 2
-            formula += inputOne
-            logic()
-        } 
-    }
-    else if (operation == 1) {
-        if (formula.length <= 11) {
-            inputTwo += 2
-            formula += inputTwo
-            logic()
+        else if (bool == false || op > 0) {
+            op == 0 ? (display.innerText = '', op++) : display ; 
+            log(item.value) 
         }
+    })
+})
+
+// Finding the value type and calling the appropriate func()
+let ref = ''
+type = 0
+function log(val) {
+    if (bool == false || type == 0) {
+        bool ? (ref = val, op++): ref ;
+        val == '=' ? opperation() : display.innerText += val ;
+        bool ? type++ : type ;
     }
 }
 
-function three () {
-    inputOne = [];
-    inputTwo = [];
-    if (operation == 0) {
-        if (formula.length <= 5) {
-            inputOne += 3
-            formula += inputOne
-            logic()
-        } 
+function opperation() {
+    let formula = display.innerText.replace(/[*+-]/g, '?').replace('/', '?').split('?')
+    let first = parseFloat(formula[0])
+    let second = parseFloat(formula[1])
+    switch (ref) {
+        case '+':
+            output = first + second
+            break;
+        case '-':
+            output = first - second
+            break;
+        case '*':
+            output = first * second
+            break;
+        case '/':
+            output = first / second
+            break;
     }
-    else if (operation == 1) {
-        if (formula.length <= 11) {
-            inputTwo += 3
-            formula += inputTwo
-            logic()
-        }
-    }
+    display.innerText = output
+    op = -1
+    type = 0
 }
 
-function four () {
-    inputOne = [];
-    inputTwo = [];
-    if (operation == 0) {
-        if (formula.length <= 5) {
-            inputOne += 4
-            formula += inputOne
-            logic()
-        } 
-    }
-    else if (operation == 1) {
-        if (formula.length <= 11) {
-            inputTwo += 4
-            formula += inputTwo
-            logic()
-        }
-    }
-}
-
-function five () {
-    inputOne = [];
-    inputTwo = [];
-    if (operation == 0) {
-        if (formula.length <= 5) {
-            inputOne += 5
-            formula += inputOne
-            logic()
-        } 
-    }
-    else if (operation == 1) {
-        if (formula.length <= 11) {
-            inputTwo += 5
-            formula += inputTwo
-            logic()
-        }
-    }
-}
-
-function six () {
-    inputOne = [];
-    inputTwo = [];
-    if (operation == 0) {
-        if (formula.length <= 5) {
-            inputOne += 6
-            formula += inputOne
-            logic()
-        } 
-    }
-    else if (operation == 1) {
-        if (formula.length <= 11) {
-            inputTwo += 6
-            formula += inputTwo
-            logic()
-        }
-    }
-}
-
-function seven () {
-    inputOne = [];
-    inputTwo = [];
-    if (operation == 0) {
-        if (formula.length <= 5) {
-            inputOne += 7
-            formula += inputOne
-            logic()
-        } 
-    }
-    else if (operation == 1) {
-        if (formula.length <= 11) {
-            inputTwo += 7
-            formula += inputTwo
-            logic()
-        }
-    }
-}
-
-function eight () {
-    inputOne = [];
-    inputTwo = [];
-    if (operation == 0) {
-        if (formula.length <= 5) {
-            inputOne += 8
-            formula += inputOne
-            logic()
-        } 
-    }
-    else if (operation == 1) {
-        if (formula.length <= 11) {
-            inputTwo += 8
-            formula += inputTwo
-            logic()
-        }
-    }
-}
-
-function nine () {
-    inputOne = [];
-    inputTwo = [];
-    if (operation == 0) {
-        if (formula.length <= 5) {
-            inputOne += 9
-            formula += inputOne
-            logic()
-        } 
-    }
-    else if (operation == 1) {
-        if (formula.length <= 11) {
-            inputTwo += 9
-            formula += inputTwo
-            logic()
-        }
-    }
-}
-
-function zero () {
-    inputOne = [];
-    inputTwo = [];
-    if (operation == 0) {
-        if (formula.length <= 5) {
-            inputOne += 0
-            formula += inputOne
-            logic()
-        } 
-    }
-    else if (operation == 1) {
-        if (formula.length <= 11) {
-            inputTwo += 0
-            formula += inputTwo
-            logic()
-        }
-    }
-}
-
-function dot () {
-    inputOne = [];
-    inputTwo = [];
-    if (operation == 0) {
-        if (formula.length <= 5) {
-            inputOne += '.'
-            formula += inputOne
-            logic()
-        } 
-    }
-    else if (operation == 1) {
-        if (formula.length <= 11) {
-            inputTwo += '.'
-            formula += inputTwo
-            logic()
-        }
-    }
-}
-
-function plus () {
-    if (operation == 0) {
-    operator = '+'
-    formula += operator
-    operation = 1
-    logic()
-    }
-}
-
-function minus () {
-    if (operation == 0) {
-    operator = '-'
-    formula += operator
-    operation = 1
-    logic()
-    }
-}
-
-function times() {
-    if (operation == 0) {
-        operator = '*'
-        formula += operator
-        operation = 1
-        logic()
-    }
-}
-
-function change() {
-    if (operation == 0) {
-        if (formula == 0) {
-            console.log('Not Valid')
-        }
-        formula -= formula*2
-        logic()  
-    }
-    else if (operation == 1) {
-        formula = 'Cant do that'
-        logic()
-    }
-}
-
-function percent() {
-    if (operation == 0) {
-        if (formula == 0) {
-            console.log('Not Valid')
-        }
-        formula = formula/100
-        logic()  
-    }
-    else if (operation == 1) {
-        formula = 'Cant do that'
-        logic()
-    }
-}
-
-function devide() {
-    if (operation == 0) {
-        operator = '/'
-        formula += operator
-        operation = 1
-        logic()
-    }
-}
-
-function whipe () {
-    operation = 0;
-    inputOne = [];
-    inputTwo = [];
-    operator = [];
-    formula = '';
-    output = 0; 
-    placeHolder.innerHTML = 0
-}
-
-
-function equal () {
-    if (operator == '+') {
-        output = formula.replace('+', ',')
-        let value = output.split(',')
-        let valueOne = parseFloat(value[0])
-        let valueTwo = parseFloat(value[1])
-        output = valueOne + valueTwo
-        placeHolder.innerHTML = output
-        formula = '';
-        formula += output.toString()
-        valueOne = 0
-        valueTwo = 0
-        operation = 0
-    }
-    else if (operator == '-') {
-        output = formula.replace('-', ',')
-        let value = output.split(',')
-        let valueOne = parseInt(value[0])
-        let valueTwo = parseInt(value[1])
-        output = valueOne - valueTwo
-        console.log(output)
-        placeHolder.innerHTML = output
-        formula = '';
-        formula += output.toString()
-        valueOne = 0
-        valueTwo = 0
-        operation = 0
-    }
-    else if (operator == '*') {
-        output = formula.replace('*', ',')
-        let value = output.split(',')
-        let valueOne = parseInt(value[0])
-        let valueTwo = parseInt(value[1])
-        output = valueOne * valueTwo
-        console.log(output)
-        placeHolder.innerHTML = output
-        formula = '';
-        formula += output.toString()
-        valueOne = 0
-        valueTwo = 0
-        operation = 0
-    }
-    else if (operator == '/') {
-        output = formula.replace('/', ',')
-        let value = output.split(',')
-        let valueOne = parseInt(value[0])
-        let valueTwo = parseInt(value[1])
-        output = valueOne / valueTwo
-        console.log(output)
-        placeHolder.innerHTML = output
-        formula = '';
-        formula += output.toString()
-        valueOne = 0
-        valueTwo = 0
-        operation = 0
-    }
-}
-
-function logic () {
-    placeHolder.innerHTML = formula
+function clear() {
+    display.innerText = output
+    op = 0
+    type = 0
 }
