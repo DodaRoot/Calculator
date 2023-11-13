f29f5b8130b62cc3d0dde2435906a24b67946fd9
Dode Doda <root.doda@gmail.com>
2023-11-13 15:40:30 +0100

    Updated the UI and added more features

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ let display = document.querySelector('.display')
 let btn = document.querySelectorAll('#btn')
 let op = 0
 let bool = false
-// Default display output
-let output = 0
 
 // Geting the value from the calc buttons
 btn.forEach((item) => {
@@ -18,6 +16,10 @@ btn.forEach((item) => {
             op == 0 ? (display.innerText = '', op++) : display ; 
             log(item.value) 
         }
+        let font = display.innerText.length
+        font > 9 ? display.style.fontSize = '50px' : false ;
+        font > 11 ? display.style.fontSize = '40px' : false ;
+        font > 15 ? display.style.fontSize = '30px' : false ;
     })
 })
 
@@ -25,13 +27,22 @@ btn.forEach((item) => {
 let ref = ''
 type = 0
 function log(val) {
-    if (bool == false || type == 0) {
+    if (['CE', 'DEL', '+/-', '%', '.'].includes(val)) {
+        val == 'CE' ? clear() : false ;
+        val == 'DEL' ? deletion() : false;
+        val == '+/-' ? negation() : false ;
+        val == '%' ? percent() : false;
+        val == '.' ? dot(val) : false ;
+    }
+    else if (bool == false || type == 0) {
         bool ? (ref = val, op++): ref ;
         val == '=' ? opperation() : display.innerText += val ;
         bool ? type++ : type ;
     }
 }
 
+// The +, -, *, / opperations func()
+let output = 0
 function opperation() {
     let formula = display.innerText.replace(/[*+-]/g, '?').replace('/', '?').split('?')
     let first = parseFloat(formula[0])
@@ -50,13 +61,48 @@ function opperation() {
             output = first / second
             break;
     }
+    display.style.fontSize = '60px'
     display.innerText = output
     op = -1
     type = 0
+    count1 = 0
+    count2 = 0
 }
 
+// CE opperation func()
 function clear() {
-    display.innerText = output
+    display.style.fontSize = '60px'
+    display.innerText = '0'
     op = 0
     type = 0
+}
+
+// DEL opperation func()
+function deletion() {
+    let lastChar = display.innerText
+    let index = lastChar[lastChar.length - 1]
+    if (['+', '-', '*', '/'].includes(index)) {
+        type = 0
+    }
+    let deleted = display.innerText.slice(0, -1) 
+    display.innerText = deleted
+}
+
+function percent() {
+    val = display.innerText
+    val = val / 100
+    display.innerText = val
+}
+
+let count1 = 0
+let count2 = 0
+function dot (val) {
+    if (op < 2) {
+        count1 == 0 ? display.innerText += val : false ;
+        count1++
+    }
+    else {
+        count2 == 0 ? display.innerText += val : false ;
+        count2++
+    }
 }
